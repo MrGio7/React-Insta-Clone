14649551d1f8efce6be9f94b9c110a43b5e90197
MrGio7 <lashagiorgi1233@gmail.com>
2019-04-19 22:19:21 +0400

    created PostsPage.js and moved in: App.js data

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,44 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
-import DummyDatta from './dummy-data';
 
-import PostContainer from './components/PostContainer/PostContainer';
-import SearchBar from './components/SearchBar/SearchBar';
+import PostsPage from './components/PostContainer/PostsPage'
 
 class App extends Component {
   constructor(props){
     super(props);
 
     this.state = {
-      posts: [],
-      filteredPosts: []
     }
-  }
-
-  searchHandle = e =>{
-    let posts = this.state.posts.filter(p => {
-      return p.username.includes(e.target.value)
-    });
-    this.setState({filteredPosts: posts})
-  }
-
-  componentDidMount() {
-    this.setState ({
-      posts: DummyDatta,
-      filteredPosts: DummyDatta
-    })
   }
 
   render() {
     console.log(this.state)
     return (
       <div className="App">
-        <div className='header-nav'>
-          <SearchBar
-            search={this.searchHandle}
-          />
-        </div>
-        <PostContainer posts={this.state.filteredPosts} />
+        <PostsPage />
       </div>
     );
   }
---
+++ instagram-app/src/components/PostContainer/PostsPage.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import DummyDatta from '../dummy-data';
+import PostContainer from './PostContainer';
+import SearchBar from '../SearchBar/SearchBar';
+
+class PostsPage extends React.Component {
+    constructor(props){
+      super(props);
+  
+      this.state = {
+        posts: [],
+        filteredPosts: []
+      }
+    }
+  
+    searchHandle = e =>{
+      let posts = this.state.posts.filter(p => {
+        return p.username.includes(e.target.value)
+      });
+      this.setState({filteredPosts: posts})
+    }
+  
+    componentDidMount() {
+      this.setState ({
+        posts: DummyDatta,
+        filteredPosts: DummyDatta
+      })
+    }
+  
+    render() {
+      console.log(this.state)
+      return (
+        <div className="App">
+          <div className='header-nav'>
+            <SearchBar
+              search={this.searchHandle}
+            />
+          </div>
+          <PostContainer posts={this.state.filteredPosts} />
+        </div>
+      );
+    }
+  }
+  
+  export default PostsPage;
+  
