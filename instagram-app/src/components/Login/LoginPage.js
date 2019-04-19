19cf64f7d96c23742033b1554b7f17430ccb05a5
MrGio7 <lashagiorgi1233@gmail.com>
2019-04-19 23:30:14 +0400

    updated handleChanges function for username and password in loginpage

--- instagram-app/src/components/Login/LoginPage.js
+++ instagram-app/src/components/Login/LoginPage.js
@@ -5,10 +5,18 @@ class LoginPage extends React.Component{
     constructor(){
         super();
 
-        this.state={};
+        this.state={
+            username: '',
+            password: ''
+        };
+    }
+
+    handleChanges = e =>{
+        this.setState({ [e.target.name]: e.target.value});
     }
 
     render(){
+        console.log(this.state.password)
         return(
             <div className='login-page'>
                 <h1>Login Form:</h1>
@@ -17,7 +25,13 @@ class LoginPage extends React.Component{
                     <h4>UserName:</h4>
 
                     <form>
-                        <input type='text' placeholder='enter username'></input>
+                        <input 
+                            type='text' 
+                            placeholder='enter username' 
+                            name='username' 
+                            value={this.state.username}
+                            onChange={this.handleChanges}
+                        />
                     </form>
 
                 </div>
@@ -26,7 +40,13 @@ class LoginPage extends React.Component{
                     <h4>Password:</h4>
 
                     <form>
-                        <input type='text' placeholder='enter password'></input>
+                        <input 
+                            type='text' 
+                            placeholder='enter password' 
+                            name='password'
+                            value={this.state.password}
+                            onChange={this.handleChanges}
+                        />
                     </form>
                 </div>
 
